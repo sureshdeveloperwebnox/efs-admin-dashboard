731c1a897d8a50b399a4063a31cc816137e56028
sureshdeveloperwebnox <sureshdeveloperwebnox@gmail.com>
2025-05-28 14:46:12 +0530

    Dcokerfile modified

--- src/views/WorkOrder/ViewWorkOrder.tsx
+++ src/views/WorkOrder/ViewWorkOrder.tsx
@@ -13,17 +13,17 @@ import TableBody from '@mui/material/TableBody';
 import TableRow from '@mui/material/TableRow';
 import TableCell from '@mui/material/TableCell';
 import BackButton from 'components/CustomComponents/BackButton';
-import { GetAssetService, GetWorkOrderService } from 'api/services';
+import { GetWorkOrderService } from 'api/services';
 import dayjs from 'dayjs';
 import { Chip, TableHead } from '@mui/material';
-import { 
-  FaFileAlt, 
-  FaCalendarAlt, 
-  FaCalendarCheck, 
-  FaDollarSign, 
-  FaMapMarkerAlt,
-  FaFlag,
-  FaExclamation
+import {
+    FaFileAlt,
+    FaCalendarAlt,
+    FaCalendarCheck,
+    FaDollarSign,
+    FaMapMarkerAlt,
+    FaFlag,
+    FaExclamation
 } from 'react-icons/fa';
 
 export default function ViewWorkOrder() {
@@ -67,175 +67,175 @@ export default function ViewWorkOrder() {
                 <BackButton />
 
 
-<MainCard title="Work Order Details">
-  <Grid container spacing={2}>
-    {/* Left Column */}
-    <Grid item xs={12} md={6}>
-      <TableContainer>
-        <Table>
-          <TableBody>
-            <TableRow>
-              <TableCell sx={{ display: 'flex', alignItems: 'center', gap: 1, width: '60%' }}>
-                <FaFileAlt size={16} />
-                <Typography variant="button">Work Order Name</Typography>
-              </TableCell>
-              <TableCell>
-                <Typography variant="subtitle1">{data?.title || '-'}</Typography>
-              </TableCell>
-            </TableRow>
+                <MainCard title="Work Order Details">
+                    <Grid container spacing={2}>
+                        {/* Left Column */}
+                        <Grid item xs={12} md={6}>
+                            <TableContainer>
+                                <Table>
+                                    <TableBody>
+                                        <TableRow>
+                                            <TableCell sx={{ display: 'flex', alignItems: 'center', gap: 1, width: '60%' }}>
+                                                <FaFileAlt size={16} />
+                                                <Typography variant="button">Work Order Name</Typography>
+                                            </TableCell>
+                                            <TableCell>
+                                                <Typography variant="subtitle1">{data?.title || '-'}</Typography>
+                                            </TableCell>
+                                        </TableRow>
 
-            <TableRow>
-              <TableCell sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-                <FaFlag size={16} />
-                <Typography variant="button">Status</Typography>
-              </TableCell>
-              <TableCell>
-                <Typography color="primary" variant="subtitle1">
-                  {<Chip label={data?.status}/>}
-                </Typography>
-              </TableCell>
-            </TableRow>
+                                        <TableRow>
+                                            <TableCell sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+                                                <FaFlag size={16} />
+                                                <Typography variant="button">Status</Typography>
+                                            </TableCell>
+                                            <TableCell>
+                                                <Typography color="primary" variant="subtitle1">
+                                                    {<Chip label={data?.status} />}
+                                                </Typography>
+                                            </TableCell>
+                                        </TableRow>
 
-            <TableRow>
-              <TableCell sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-                <FaExclamation size={16} />
-                <Typography variant="button">Priority</Typography>
-              </TableCell>
-              <TableCell>
-                <Typography color="primary" variant="subtitle1">
-               {<Chip label={data?.priority}/>}
+                                        <TableRow>
+                                            <TableCell sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+                                                <FaExclamation size={16} />
+                                                <Typography variant="button">Priority</Typography>
+                                            </TableCell>
+                                            <TableCell>
+                                                <Typography color="primary" variant="subtitle1">
+                                                    {<Chip label={data?.priority} />}
 
-                </Typography>
-              </TableCell>
-            </TableRow>
+                                                </Typography>
+                                            </TableCell>
+                                        </TableRow>
 
-            <TableRow>
-              <TableCell sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-                <FaCalendarAlt size={16} />
-                <Typography variant="button">Scheduled Start</Typography>
-              </TableCell>
-              <TableCell>
-                <Typography color="info" variant="subtitle1">
-                  {dayjs(data?.scheduled_start_date).format('DD-MM-YYYY') || '-'}
-                </Typography>
-              </TableCell>
-            </TableRow>
+                                        <TableRow>
+                                            <TableCell sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+                                                <FaCalendarAlt size={16} />
+                                                <Typography variant="button">Scheduled Start</Typography>
+                                            </TableCell>
+                                            <TableCell>
+                                                <Typography color="info" variant="subtitle1">
+                                                    {dayjs(data?.scheduled_start_date).format('DD-MM-YYYY') || '-'}
+                                                </Typography>
+                                            </TableCell>
+                                        </TableRow>
 
-            <TableRow>
-              <TableCell sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-                <FaCalendarAlt size={16} />
-                <Typography variant="button">Scheduled End</Typography>
-              </TableCell>
-              <TableCell>
-                <Typography color="info" variant="subtitle1">
-                  {dayjs(data?.scheduled_end_date).format('DD-MM-YYYY') || '-'}
-                </Typography>
-              </TableCell>
-            </TableRow>
+                                        <TableRow>
+                                            <TableCell sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+                                                <FaCalendarAlt size={16} />
+                                                <Typography variant="button">Scheduled End</Typography>
+                                            </TableCell>
+                                            <TableCell>
+                                                <Typography color="info" variant="subtitle1">
+                                                    {dayjs(data?.scheduled_end_date).format('DD-MM-YYYY') || '-'}
+                                                </Typography>
+                                            </TableCell>
+                                        </TableRow>
 
-           
-                <TableRow>
-              <TableCell sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-                <FaCalendarCheck size={16} />
-                <Typography variant="button">Actual Start</Typography>
-              </TableCell>
-              <TableCell>
-                <Typography color="success" variant="subtitle1">
-                  {dayjs(data?.actual_start_date).format('DD-MM-YYYY') || '-'}
-                </Typography>
-              </TableCell>
-            </TableRow>
 
-            <TableRow>
-              <TableCell sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-                <FaCalendarCheck size={16} />
-                <Typography variant="button">Actual End</Typography>
-              </TableCell>
-              <TableCell>
-                <Typography color="success" variant="subtitle1">
-                  {dayjs(data?.actual_end_date).format('DD-MM-YYYY') || '-'}
-                </Typography>
-              </TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </Grid>
+                                        <TableRow>
+                                            <TableCell sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+                                                <FaCalendarCheck size={16} />
+                                                <Typography variant="button">Actual Start</Typography>
+                                            </TableCell>
+                                            <TableCell>
+                                                <Typography color="success" variant="subtitle1">
+                                                    {dayjs(data?.actual_start_date).format('DD-MM-YYYY') || '-'}
+                                                </Typography>
+                                            </TableCell>
+                                        </TableRow>
 
-    {/* Right Column */}
-    <Grid item xs={12} md={6}>
-      <TableContainer>
-        <Table>
-          <TableBody>
-         <TableRow>
-              <TableCell sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-                <FaDollarSign size={16} />
-                <Typography variant="button">Estimated Cost</Typography>
-              </TableCell>
-              <TableCell>
-                <Typography variant="subtitle1">
-                  {data?.estimated_cost || '-'}
-                </Typography>
-              </TableCell>
-            </TableRow>
+                                        <TableRow>
+                                            <TableCell sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+                                                <FaCalendarCheck size={16} />
+                                                <Typography variant="button">Actual End</Typography>
+                                            </TableCell>
+                                            <TableCell>
+                                                <Typography color="success" variant="subtitle1">
+                                                    {dayjs(data?.actual_end_date).format('DD-MM-YYYY') || '-'}
+                                                </Typography>
+                                            </TableCell>
+                                        </TableRow>
+                                    </TableBody>
+                                </Table>
+                            </TableContainer>
+                        </Grid>
 
-            <TableRow>
-              <TableCell sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-                <FaDollarSign size={16} />
-                <Typography variant="button">Actual Cost</Typography>
-              </TableCell>
-              <TableCell>
-                <Typography variant="subtitle1">
-                  {data?.actual_cost || '-'}
-                </Typography>
-              </TableCell>
-            </TableRow>
+                        {/* Right Column */}
+                        <Grid item xs={12} md={6}>
+                            <TableContainer>
+                                <Table>
+                                    <TableBody>
+                                        <TableRow>
+                                            <TableCell sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+                                                <FaDollarSign size={16} />
+                                                <Typography variant="button">Estimated Cost</Typography>
+                                            </TableCell>
+                                            <TableCell>
+                                                <Typography variant="subtitle1">
+                                                    {data?.estimated_cost || '-'}
+                                                </Typography>
+                                            </TableCell>
+                                        </TableRow>
 
-            <TableRow>
-              <TableCell sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-                <FaMapMarkerAlt size={16} />
-                <Typography variant="button">Address</Typography>
-              </TableCell>
-              <TableCell>
-                <Typography variant="subtitle1">{data?.address || '-'}</Typography>
-              </TableCell>
-            </TableRow>
+                                        <TableRow>
+                                            <TableCell sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+                                                <FaDollarSign size={16} />
+                                                <Typography variant="button">Actual Cost</Typography>
+                                            </TableCell>
+                                            <TableCell>
+                                                <Typography variant="subtitle1">
+                                                    {data?.actual_cost || '-'}
+                                                </Typography>
+                                            </TableCell>
+                                        </TableRow>
 
-            <TableRow>
-              <TableCell sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-                <FaMapMarkerAlt size={16} />
-                <Typography variant="button">State</Typography>
-              </TableCell>
-              <TableCell>
-                <Typography variant="subtitle1">{data?.state || '-'}</Typography>
-              </TableCell>
-            </TableRow>
+                                        <TableRow>
+                                            <TableCell sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+                                                <FaMapMarkerAlt size={16} />
+                                                <Typography variant="button">Address</Typography>
+                                            </TableCell>
+                                            <TableCell>
+                                                <Typography variant="subtitle1">{data?.address || '-'}</Typography>
+                                            </TableCell>
+                                        </TableRow>
 
-            <TableRow>
-              <TableCell sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-                <FaMapMarkerAlt size={16} />
-                <Typography variant="button">City</Typography>
-              </TableCell>
-              <TableCell>
-                <Typography variant="subtitle1">{data?.city || '-'}</Typography>
-              </TableCell>
-            </TableRow>
+                                        <TableRow>
+                                            <TableCell sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+                                                <FaMapMarkerAlt size={16} />
+                                                <Typography variant="button">State</Typography>
+                                            </TableCell>
+                                            <TableCell>
+                                                <Typography variant="subtitle1">{data?.state || '-'}</Typography>
+                                            </TableCell>
+                                        </TableRow>
 
-            <TableRow>
-              <TableCell sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-                <FaMapMarkerAlt size={16} />
-                <Typography variant="button">Postal Code</Typography>
-              </TableCell>
-              <TableCell>
-                <Typography variant="subtitle1">{data?.postal_code || '-'}</Typography>
-              </TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </Grid>
-  </Grid>
-</MainCard>
+                                        <TableRow>
+                                            <TableCell sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+                                                <FaMapMarkerAlt size={16} />
+                                                <Typography variant="button">City</Typography>
+                                            </TableCell>
+                                            <TableCell>
+                                                <Typography variant="subtitle1">{data?.city || '-'}</Typography>
+                                            </TableCell>
+                                        </TableRow>
+
+                                        <TableRow>
+                                            <TableCell sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+                                                <FaMapMarkerAlt size={16} />
+                                                <Typography variant="button">Postal Code</Typography>
+                                            </TableCell>
+                                            <TableCell>
+                                                <Typography variant="subtitle1">{data?.postal_code || '-'}</Typography>
+                                            </TableCell>
+                                        </TableRow>
+                                    </TableBody>
+                                </Table>
+                            </TableContainer>
+                        </Grid>
+                    </Grid>
+                </MainCard>
             </Grid>
 
             <Grid item xs={12} md={6}>
